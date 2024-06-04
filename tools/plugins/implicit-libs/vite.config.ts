f162d404e519242b84b870b48257c7b3550f6144
Younes Jaaidi <younes@marmicode.io>
2024-06-05 01:12:46 +0200

    build: 📦 setup implicit lib plugin project
    nx add @nx/plugin
    nx g plugin implicit-libs --directory tools/plugins/implicit-libs --unitTestRunner none
    nx g @nx/vite:vitest --project implicit-libs

---
+++ tools/plugins/implicit-libs/vite.config.ts
@@ -0,0 +1,24 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../../node_modules/.vite/tools/plugins/implicit-libs',
+
+  plugins: [nxViteTsPaths()],
+
+  test: {
+    watch: false,
+    globals: true,
+    environment: 'node',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../../coverage/tools/plugins/implicit-libs',
+      provider: 'v8',
+    },
+  },
+});
