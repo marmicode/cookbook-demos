2416e5ff327240a6440cddc3121d26cc32eb1451
Younes Jaaidi <younes@marmicode.io>
2026-01-15 15:29:39 +0100

    refactor(vitest-browser-mode): 🛠️ disable oversized image warning

--- apps/demo/src/app/app.config.ts
+++ apps/demo/src/app/app.config.ts
@@ -1,7 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { IMAGE_CONFIG } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    /* For convenience, we are using oversized online images in the demo
+     * instead of downloading them and resizing them. */
+    { provide: IMAGE_CONFIG, useValue: { disableImageSizeWarning: true } },
+  ],
 };
