5d1c3c6d7da3b9abc480e97ebaa6afa3f65245ef
Younes Jaaidi <younes@marmicode.io>
2026-01-21 15:38:07 +0100

    tmp

--- apps/demo/src/app/vitest-browser-mode/cookbook-search.spec.ts
+++ apps/demo/src/app/vitest-browser-mode/cookbook-search.spec.ts
@@ -1,4 +1,8 @@
 import { describe, it, expect } from 'vitest';
 import { CookbookSearch } from './cookbook-search.ng';
 
-describe(CookbookSearch.name, () => {});
+describe(CookbookSearch.name, () => {
+  it.todo('filters cookbooks by author name');
+
+  it.todo('adds cookbook to the cart when user clicks on "Add to Cart" button');
+});
