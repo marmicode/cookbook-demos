1ef32d03fd388fbe59f1f6676fc0761667a5d951
Younes Jaaidi <younes@marmicode.io>
2026-01-21 16:39:07 +0100

    tmp

--- apps/demo/src/app/vitest-browser-mode/cookbook-search.spec.ts
+++ apps/demo/src/app/vitest-browser-mode/cookbook-search.spec.ts
@@ -1,8 +1,18 @@
+import { TestBed } from '@angular/core/testing';
 import { describe, it, expect } from 'vitest';
 import { CookbookSearch } from './cookbook-search.ng';
+import { screen } from '@testing-library/angular';
+import { userEvent } from '@testing-library/user-event';
 
 describe(CookbookSearch.name, () => {
-  it.todo('filters cookbooks by author name');
+  it('filters cookbooks by author name', async () => {
+    TestBed.createComponent(CookbookSearch);
+
+    const keywordsEl = await screen.findByRole('textbox', { name: 'Keywords' });
+    await userEvent.type(keywordsEl, 'Marmicode');
+
+    expect(await screen.findAllByRole('heading')).toHaveLength(3);
+  });
 
   it.todo('adds cookbook to the cart when user clicks on "Add to Cart" button');
 });
