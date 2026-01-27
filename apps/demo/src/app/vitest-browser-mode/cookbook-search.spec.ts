1a7de719727e82781675f5452fa23aed628ae9dc
Younes Jaaidi <younes@marmicode.io>
2026-01-27 10:28:48 +0100

    Revert "tmp"
    This reverts commit 327d893f274572db5b0c521e15f3f4752771859c.

--- apps/demo/src/app/vitest-browser-mode/cookbook-search.spec.ts
+++ apps/demo/src/app/vitest-browser-mode/cookbook-search.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { describe, it, expect, vi } from 'vitest';
-import { Cart } from './cart';
 import { CookbookSearch } from './cookbook-search.ng';
 import { screen } from '@testing-library/angular';
 import { userEvent } from '@testing-library/user-event';
@@ -21,19 +20,5 @@ describe(CookbookSearch.name, () => {
     });
   });
 
-  it('adds cookbook to the cart when user clicks on "Add to Cart" button', async () => {
-    const cart = TestBed.inject(Cart);
-    TestBed.createComponent(CookbookSearch);
-
-    const addToCartButtons = await screen.findAllByRole('button', {
-      name: 'Add to Cart',
-    });
-    await userEvent.click(addToCartButtons[0]);
-
-    expect(cart.cookbooks()).toMatchObject([
-      {
-        title: 'Angular Testing Cookbook',
-      },
-    ]);
-  });
+  it.todo('adds cookbook to the cart when user clicks on "Add to Cart" button');
 });
