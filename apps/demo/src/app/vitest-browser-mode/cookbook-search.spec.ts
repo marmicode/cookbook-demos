87071305f3ccfa8c36439e52c4c98ba2f5b7e376
Younes Jaaidi <younes@marmicode.io>
2026-01-27 10:31:38 +0100

    tmp

--- apps/demo/src/app/vitest-browser-mode/cookbook-search.spec.ts
+++ apps/demo/src/app/vitest-browser-mode/cookbook-search.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { describe, it, expect, vi } from 'vitest';
+import { Cart } from './cart';
 import { CookbookSearch } from './cookbook-search.ng';
 import { screen } from '@testing-library/angular';
 import { userEvent } from '@testing-library/user-event';
@@ -20,5 +21,19 @@ describe(CookbookSearch.name, () => {
     });
   });
 
-  it.todo('adds cookbook to the cart when user clicks on "Add to Cart" button');
+  it('adds cookbook to the cart when user clicks on "Add to Cart" button', async () => {
+    const cart = TestBed.inject(Cart);
+    TestBed.createComponent(CookbookSearch);
+
+    const addToCartButtons = await screen.findAllByRole('button', {
+      name: 'Add to Cart',
+    });
+    await userEvent.click(addToCartButtons[0]);
+
+    expect(cart.cookbooks()).toMatchObject([
+      {
+        title: 'Angular Testing Cookbook',
+      },
+    ]);
+  });
 });
