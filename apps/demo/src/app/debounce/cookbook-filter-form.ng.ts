b325afe3f3021d314217f45a5c56563bd3466c82
Younes Jaaidi <younes@marmicode.io>
2026-03-16 16:36:58 +0100

    refactor: 🛠️ add debounce

--- apps/demo/src/app/debounce/cookbook-filter-form.ng.ts
+++ apps/demo/src/app/debounce/cookbook-filter-form.ng.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, model } from '@angular/core';
-import { form, FormField } from '@angular/forms/signals';
+import { debounce, form, FormField } from '@angular/forms/signals';
 import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
 
 @Component({
@@ -19,11 +19,20 @@ import { MatFormField, MatInput, MatLabel } from '@angular/material/input';
   `,
 })
 export class CookbookFilterForm {
-  filter = model<CookbookFilter>({ keywords: '' });
+  filter = model(createCookbookFilter());
 
-  filterForm = form(this.filter);
+  filterForm = form(this.filter, (path) => debounce(path.keywords, 300));
 }
 
 export interface CookbookFilter {
   keywords: string;
+}
+
+export function createCookbookFilter(
+  filter: Partial<CookbookFilter> = {},
+): CookbookFilter {
+  return {
+    keywords: '',
+    ...filter,
+  };
 }
